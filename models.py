ffd15885238e108b5e3d17317a85cad9b475ca0f
Jan <jan.hula@cvut.cz>
2024-09-10 09:12:05 +0200

    multiple modules

--- models.py
+++ models.py
@@ -114,6 +114,7 @@ class GPTConfig:
     block_size: int = 1024
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12
+    n_iters: int = 1
     n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 from urllib.request import urlopen
 
 import pickle
-
+import logging
 import lightning as L
 import torch
 from torch.utils.data import DataLoader
@@ -12,32 +12,33 @@ from lightning.pytorch.loggers import WandbLogger
 import functools
 import warnings
 from typing import Any, Optional, Tuple
-
+from omegaconf import DictConfig, OmegaConf
+import hydra
 import torch.optim
 from lightning import LightningModule
 import models
+import modular_models
+
+log = logging.getLogger(__name__)
 
 class NanoGPT(LightningModule):
 
     def __init__(
         self,
-        vocab_size: int,
-        block_size: int,
-        n_layer: Optional[int] = None,
-        n_head: Optional[int] = None,
-        n_embd: Optional[int] = None,
+        cfg: DictConfig,
         dropout: float = 0.0,
         weight_decay: float = 0.1,
         learning_rate: float = 3e-4,
-        betas: Tuple[float, float] = (0.9, 0.95)
+        betas: Tuple[float, float] = (0.9, 0.95),
     ):
         super().__init__()
         self.betas = betas
         self.weight_decay = weight_decay
         self.learning_rate = learning_rate
         self.save_hyperparameters()
-        self.config = models.GPTConfig(vocab_size=vocab_size, block_size=block_size, n_layer=n_layer, n_head=n_head, n_embd=n_embd, dropout=dropout)
-        self.gpt = models.GPT(self.config)
+        #self.config = models.GPTConfig(vocab_size=vocab_size, block_size=block_size, n_iters=n_iters, n_head=n_head, n_embd=n_embd, dropout=dropout)
+        #self.gpt = models.GPT(self.config)
+        self.gpt = modular_models.ModularGPT(cfg.model)
         self.validation_step_outputs = []
 
 
@@ -97,14 +98,14 @@ def get_data(data, input_length, flag):
     y = torch.zeros((num_samples, 1), dtype=torch.long)
     # Fill x and y with data
     for i, sample in enumerate(dataset):
-        print(i, sample)
+        #print(i, sample)
         x[i] = torch.tensor(sample['input'])
         y[i] = torch.tensor([sample['target_idx']])
     return x, y
 
-
-def main(args):
-    with open ("/mnt/raid/data/Hyner_Petr/rl/rl_basic_transformer/new_data_only_triples.pkl", "rb") as f:
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def main(cfg):
+    with open (cfg.data.dataset_path, "rb") as f:
         data = pickle.load(f)
     # create torch dataset for train and test
     train_data = get_data(data, 20, False) 
@@ -114,20 +115,20 @@ def main(args):
     train_dataset = torch.utils.data.TensorDataset(train_data[0], train_data[1])
     test_dataset = torch.utils.data.TensorDataset(test_data[0], test_data[1])
 
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers, shuffle=False)
 
     callback_list = []
-    model = NanoGPT(vocab_size=106,block_size=args.block_size,n_layer=args.n_layer,n_head=args.n_head,n_embd=args.n_embd)
+    model = NanoGPT(cfg)
 
-    wandb_logger = WandbLogger(project=args.project_name, name=args.data_folder, save_dir=args.data_folder)
-    trainer = L.Trainer.from_argparse_args(
-        args,
-        max_epochs=args.num_epochs,
+    #wandb_logger = WandbLogger(project=args.project_name, name=args.data_folder, save_dir=args.data_folder)
+    trainer = L.Trainer(
+        #args,
+        max_epochs=cfg.train.num_epochs,
         gradient_clip_val=1.0,
         #callbacks=callback_list,
         accelerator="cuda",
-        logger=wandb_logger,
+        #logger=wandb_logger,
         #devices=args.devices,
         precision=16,
         val_check_interval=100,
@@ -136,29 +137,34 @@ def main(args):
     )
 
     trainer.fit(model, train_loader, test_loader)
+    # get the validation accuracy
+
+    accuracy = trainer.validate(model, test_loader)[0]['val_accuracy_epoch']
+    log.info(f"Model accuracy: {accuracy}")
+    return accuracy
 
 
 if __name__ == "__main__":
     L.seed_everything(42)
 
-    parser = ArgumentParser()
-    parser = L.Trainer.add_argparse_args(parser)
-    parser.add_argument("--project_name", default='cc', type=str)
-    parser.add_argument("--data_folder", default='data',type=str)
-    parser.add_argument("--n_layer", default=2,type=int)
-    parser.add_argument("--n_head", default=1,type=int)
-    parser.add_argument("--n_embd", default=16,type=int)
-    parser.add_argument("--n_hidden", default=16,type=int)
-    parser.add_argument("--tied", type=bool)
-    parser.add_argument("--num_epochs", default=100, type=int)
-    parser.add_argument("--learning_rate", default=3e-4, type=float)
-    parser.add_argument("--wdecay", default=1.2e-6, type=float)
-    parser.add_argument("--block_size", default=32, type=int)
-    parser.add_argument("--batch_size", default=64, type=int)
-    parser.add_argument("--num_workers", default=4, type=int)
-    args = parser.parse_args()
+    # parser = ArgumentParser()
+    # parser = L.Trainer.add_argparse_args(parser)
+    # parser.add_argument("--project_name", default='cc', type=str)
+    # parser.add_argument("--data_folder", default='data',type=str)
+    # parser.add_argument("--n_layer", default=2,type=int)
+    # parser.add_argument("--n_head", default=1,type=int)
+    # parser.add_argument("--n_embd", default=128,type=int)
+    # parser.add_argument("--n_hidden", default=16,type=int)
+    # parser.add_argument("--tied", type=bool)
+    # parser.add_argument("--num_epochs", default=1, type=int)
+    # parser.add_argument("--learning_rate", default=3e-4, type=float)
+    # parser.add_argument("--wdecay", default=1.2e-6, type=float)
+    # parser.add_argument("--block_size", default=32, type=int)
+    # parser.add_argument("--batch_size", default=64, type=int)
+    # parser.add_argument("--num_workers", default=4, type=int)
+    #args = parser.parse_args()
 
-    main(args)
+    main()
 
     # uložit a načíst model, když dám jeden example tak chci vidět attention mapu, kterou vytvoří první attention vrstva.
     # přes forward hook
