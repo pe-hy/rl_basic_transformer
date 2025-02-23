f83d8f573bbdc7203cf7ab6b30a0a8de73983226
= <=>
2025-02-23 01:19:08 +0100

    I am debugged

--- data_generation/generate_lines.py
+++ data_generation/generate_lines.py
@@ -4,8 +4,9 @@ from collections import defaultdict
 import random
 import pickle
 from itertools import product
+import os
 
-with open("graphs.pkl", "rb") as f:
+with open(os.path.join("data_generation", "graphs.pkl"), "rb") as f:
     graphs = pickle.load(f)
 
 
@@ -32,62 +33,108 @@ indexes = list(range(len(vecs)))
 samples = []
 
 
-def solve(sample, target_node, graphs, log=None):
-    if log is None:
-        log = []
-    target_chain = 0
-    current_state = sample.copy()
-    paths_lst = list(nx.all_simple_paths(graphs[target_chain], current_state[target_chain], target_node))
+def solve(curr_states, target_node, graphs, log=[], depth=0):
+    '''
+    Solves the problem of reaching target node from current state in graphs by returning the resulting state and log of steps.
+    Parameters:
+        curr_states: list of current states of automatas; LIST of INTEGERS
+        target_node: target node; INTEGER
+        graphs: list of graphs; LIST of networkx graphs
+        log: list of log messages; LIST of STRINGS
+        depth: depth of recursion; INTEGER
+    Returns:
+        out: states of automatas after solving or None if no solution; LIST of INTEGERS/None
+            out: log: list of log messages; LIST of STRINGS
+    '''
+    indent = ' '*depth*2
+    # log.append(f"{'-'*50}")
+    curr_states_cpy = curr_states.copy()
+    log.append(f"{indent}S {curr_states_cpy} TN {target_node}")
+    # get all paths from current state to target node
+    paths_lst = list(nx.all_simple_paths(graphs[0], curr_states_cpy[0], target_node))
     
     # check if there is a path
-    if len(paths_lst) <= 1:
+    if len(paths_lst) == 0:
         return None, log
     
-    for i, path in enumerate(paths_lst): # 3 4 5
-        log.append(f"P {path}")
+    # check if current node equals target node
+    if curr_states_cpy[0] == target_node:
+        log.append(f"{indent}REND {curr_states_cpy}")
+        return curr_states_cpy, log
+    
+    # path or paths exist and we are not in target node
+    for path in paths_lst:
+        log.append(f"{indent}P {path}")
         # projdu hrany po ceste (3-4), ziskam enabling
-        current_state = sample.copy()
-        rules = graphs[target_chain].get_edge_data(current_state[target_chain], path[1:][0])
-
+        curr_states_cpy = curr_states.copy()
+        rules = graphs[0].get_edge_data(curr_states_cpy[0], path[1])
         # pokud neexistuje enabling, projdu edge
-        log.append(f"RLS {rules}")
+        log.append(f"{indent}RLS {rules}")
         if rules == {}:
-            log.append(f"NR {current_state} TN {current_state[target_chain]}")
-            current_state[target_chain] = path[1:][0]
-            log.append(f"NRCH {current_state}")
-            return solve(current_state, target_node, graphs, log)
+            log.append(f"{indent}NR {curr_states_cpy} TN {target_node}")
+            # pokud má path jeden node, tak je to konec
+            curr_states_cpy[0] = path[1]
+            log.append(f"{indent}NRCH {curr_states_cpy}")
+
+            return solve(curr_states_cpy, target_node, graphs, log, depth+1)
 
         # pokud existuje enabling, tak treba automat 1 ma byt ve stavu 2 a automat 2 ma byt ve stavu 3
         # prvni vyresime prvni pravidlo a pak druhe
         else:
-            for rule_set in rules.values():
-                log.append(f"RS {rule_set}")
-                for rule_group in rule_set:
-                    current_state = sample.copy()
-                    log.append(f"RG {rule_group}")
-                    found = True
-                    for rule in sorted(rule_group, key=lambda x: x['automata_id']): # sorted znamena setrizene od nejvice zavisleho po nejmene
-                        log.append(f"RULE {rule}")
-                        log.append(f"R {current_state} TN {current_state[target_chain]}")
-                        current_sub_state, log = solve(current_state[target_chain:], rule['node_id'], graphs, log)
-                        log.append(f"RCH {current_sub_state}")
-                        if current_sub_state == None:
-                            log.append(f"NPATH {current_state} TN {current_state[target_chain]}")
-                            found = False
-                            break
-                        log.append(f"SUB {current_state}")
-                        current_state[target_chain:] = current_sub_state
-                        log.append(f"SUBCH {current_state}")
-                        if found:
-                            log.append(f"SUBF {current_state}")
-                            break
-                    log.append(f"REND {current_state}")
-                    current_state[target_chain] = path[1:][0]
-                    log.append(f"SUBSOLVED {current_state}")
-                    return solve(current_state, target_node, graphs, log)
-        return current_state, log
+            for rule_set in rules['enabling']:
+                log.append(f"{indent}RS {rule_set}")
+
+                curr_states_cpy = curr_states.copy()
+                found = True
+
+                for rule in sorted(rule_set, key=lambda x: x['automata_id']): # sorted znamena setrizene od nejvice zavisleho po nejmene
+                    log.append(f"{indent}RULE {rule}")
+
+                    current_sub_state, log = solve(curr_states_cpy[rule['automata_id']:], rule['node_id'], graphs[rule['automata_id']:], log, depth+1)
+
+                    log.append(f"{indent}RCH {current_sub_state}")
+                    if current_sub_state == None:
+                        log.append(f"{indent}NPATH {curr_states_cpy[rule['automata_id']:]} TN {rule['node_id']}")
+                        found = False
+                        break
+                    log.append(f"{indent}SUB {curr_states_cpy}")
+                    curr_states_cpy[rule['automata_id']:] = current_sub_state
+                    log.append(f"{indent}SUBCH {curr_states_cpy}")
+
+                if found:
+                    log.append(f"{indent}SUBF {curr_states_cpy}")
+                    curr_states_cpy[0] = path[1]
+                    log.append(f"{indent}SUBSOLVED {curr_states_cpy}")
+                    break
+                else:
+                    continue
+            
+            if found:
+                return solve(curr_states_cpy, target_node, graphs, log, depth+1)
+            else:
+                continue
+
+    return None, log
 
 
 log = []
+import pprint as pprint
+curstate, log = solve([9, 1], 1, list(graphs.values()), log, 0)
 
-curstate, log = solve([9, 1], 1, graphs, log)
+pprint.pprint(log)
+print(curstate)
+
+# # Assuming 'graphs' is a list of NetworkX graphs
+# fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+# # Plot the first graph on the first subplot
+# nx.draw(graphs[0], with_labels=True, node_color='lightblue', arrows=True, ax=axs[0])
+# axs[0].set_title('Graph 1')
+
+# # Plot the second graph on the second subplot
+# nx.draw(graphs[1], with_labels=True, node_color='lightgreen', arrows=True, ax=axs[1])
+# axs[1].set_title('Graph 2')
+
+# # Display the plots
+# plt.tight_layout()
+# plt.show()
